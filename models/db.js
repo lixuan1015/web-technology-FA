9dbde3fcf6d5f10d66d32bf9d86ef4dff272fcf2
lixuan1015 <lixuan.ching@qiu.edu.my>
2026-03-06 22:39:51 +0800

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,5 +1,11 @@
-const mysql = require("mysql2");
-const db = mysql.createConnection(process.env.MYSQL_URL);
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host:     process.env.DB_HOST,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
 db.connect(err => {
   if (err) console.error("MySQL connection error:", err);
