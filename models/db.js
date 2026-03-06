d65f4a4e5bd6201c80bd5a370274f434850bb090
lixuan1015 <lixuan.ching@qiu.edu.my>
2026-03-06 22:31:36 +0800

    Update db.js
    change command

--- models/db.js
+++ models/db.js
@@ -1,11 +1,5 @@
-const mysql = require('mysql2');
-
-const db = mysql.createConnection({
-  host:     process.env.DB_HOST,
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+const mysql = require("mysql2");
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect(err => {
   if (err) console.error("MySQL connection error:", err);
