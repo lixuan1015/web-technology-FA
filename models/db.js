f62a40395d48f1ed266eb94293d5b5723f418d2c
lixuan1015 <lixuan.ching@qiu.edu.my>
2026-03-07 17:08:31 +0800

    Update db.js
    fix: use Railway MYSQL variable names in db connection

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host:     process.env.MYSQLHOST,
-  port:     process.env.MYSQLPORT,
+  port:     process.env.MYSQLPORT || 3306,
   user:     process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE
